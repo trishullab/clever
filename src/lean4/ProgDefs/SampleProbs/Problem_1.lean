d94de9fa9ff966768c35cd19905c5b3f590e8a6d
Amitayush Thakur <amitayush@utexas.edu>
2025-02-20 19:46:09 +0000

    Fixing binary search

--- src/lean4/ProgDefs/SampleProbs/Problem_1.lean
+++ src/lean4/ProgDefs/SampleProbs/Problem_1.lean
@@ -36,7 +36,10 @@ Task 4:
 (b) Provide a termination measure for the binarySearchLowHi function
 (Points: 8)
 --/
-def binarySearchLowHi (xs: List Int) (y: Int) (low: Nat) (hi: Nat): Int :=
+def binarySearchLowHi (xs: List Int) (y: Int) (low: Nat) (hi: Nat)
+(hi_leq_low: low ≤ hi)
+(h_hi: hi < xs.length)
+: Int :=
   if low > hi then -1
   else
     let sum := low + hi;
@@ -44,6 +47,40 @@ def binarySearchLowHi (xs: List Int) (y: Int) (low: Nat) (hi: Nat): Int :=
                then sum / 2
                else (sum - 1) / 2;
     have h_i_lt_n : mid < xs.length := by
+      have h_sum_mod_2_eq_0_or_1 : sum % 2 = 0 ∨ sum % 2 = 1 := by
+        apply Nat.mod_two_eq_zero_or_one sum
+      cases h_sum_mod_2_eq_0_or_1
+      rename_i h_sum_mod_2_eq_0
+      simp [mid, h_sum_mod_2_eq_0, sum]
+      have ineq1 : low < xs.length := by
+        simp [Nat.lt_of_le_of_lt hi_leq_low h_hi]
+      have ineq2 : low + hi < xs.length + xs.length := by
+        apply Nat.add_lt_add ineq1 h_hi
+      have ineq3 : low + hi < 2 * xs.length := by
+        rw [Nat.mul_comm, Nat.mul_two]
+        exact ineq2
+      apply Nat.lt_of_lt_of_le (Nat.div_lt_of_lt_mul ineq3)
+      simp
+      rename_i h_sum_mod_2_eq_1
+      simp [mid, h_sum_mod_2_eq_1, sum]
+      have ineq1 : low < xs.length := by
+        simp [Nat.lt_of_le_of_lt hi_leq_low h_hi]
+      have ineq2 : low + hi < xs.length + xs.length := by
+        apply Nat.add_lt_add ineq1 h_hi
+      have ineq3 : low + hi < 2 * xs.length := by
+        rw [Nat.mul_comm, Nat.mul_two]
+        exact ineq2
+      have ineq4 : sum - 1 < 2 * xs.length := by
+        apply Nat.lt_of_le_of_lt (Nat.sub_le _ _)
+        exact ineq3
+      simp [sum] at ineq4
+      apply Nat.lt_of_lt_of_le (Nat.div_lt_of_lt_mul ineq4)
+      simp
+    have h_mid_leq_hi: mid + 1 ≤ hi := by
+      sorry
+    have low_leq_mid: low ≤ mid - 1 := by
+      sorry
+    have h_mid_leq_n: mid - 1 < xs.length := by
       sorry
     -- ^ NOTE: Prove the above lemma h_i_lt_n
     -- This proof is required to ensure that
@@ -52,9 +89,9 @@ def binarySearchLowHi (xs: List Int) (y: Int) (low: Nat) (hi: Nat): Int :=
     let mid_val := xs.get ⟨mid, h_i_lt_n⟩;
     if mid_val == y then mid
     else if mid_val < y then
-      binarySearchLowHi xs y (mid + 1) hi
+      binarySearchLowHi xs y (mid + 1) hi h_mid_leq_hi h_hi
     else
-      binarySearchLowHi xs y low (mid - 1)
+      binarySearchLowHi xs y low (mid - 1) low_leq_mid h_mid_leq_n
 termination_by hi - low
 decreasing_by
   simp
@@ -88,4 +125,19 @@ It returns the index of the first occurrence of a given integer in
 a sorted list. If the integer is not in the list, return -1.
 -/
 def binarySearch (xs: List Int) (y: Int): Int :=
-  binarySearchLowHi xs y 0 (xs.length - 1)
+  if h : xs.isEmpty then -1
+  else
+    let h1 : 0 ≤ xs.length - 1 := Nat.zero_le (xs.length - 1)
+    let h2 : xs.length - 1 < xs.length := by
+      simp at h
+      simp
+      by_cases xs.length = 0
+      rename_i h_len_eq_0
+      have h_xs_empty : xs = [] := by
+        apply List.eq_nil_of_length_eq_zero h_len_eq_0
+      contradiction
+      rename_i h_len_neq_0
+      have h_len_gt_0 : xs.length > 0 := by
+        apply Nat.pos_of_ne_zero h_len_neq_0
+      simp [h_len_gt_0]
+    binarySearchLowHi xs y 0 (xs.length - 1) h1 h2
