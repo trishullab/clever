49f8582e2d62b30cfce4673296c816f3828a27af
Meghana <sistla.meghana@gmail.com>
2025-04-16 13:12:56 -0700

    Removed fibonacci_4 inductive argument from P46

--- src/lean4/human_eval/problem_46.lean
+++ src/lean4/human_eval/problem_46.lean
@@ -21,17 +21,6 @@ test_cases:
 -/
 -- end_def problem_details
 
-inductive fibonacci_non_computable_4 : ℕ → ℕ → Prop
-| base0 : fibonacci_non_computable_4 0 0
-| base1 : fibonacci_non_computable_4 1 0
-| base2 : fibonacci_non_computable_4 2 2
-| base3 : fibonacci_non_computable_4 3 0
-| step : ∀ n f₁ f₂ f₃ f₄, fibonacci_non_computable_4 n f₁ →
-fibonacci_non_computable_4 (n + 1) f₂ →
-fibonacci_non_computable_4 (n + 2) f₃ →
-fibonacci_non_computable_4 (n + 3) f₄ →
-fibonacci_non_computable_4 (n + 4) (f₁ + f₂ + f₃ + f₄)
-
 --- start_def problem_spec
 def problem_spec
 -- function signature
