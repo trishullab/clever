ce2f589d49824acb2136d84f7da40b4bb2b23007
Meghana <sistla.meghana@gmail.com>
2025-04-16 14:43:25 -0700

    Specs for Problems 47-50

---
+++ src/lean4/human_eval/problem_47.lean
@@ -0,0 +1,88 @@
+import Imports.AllImports
+
+-- start_def problem_details
+/--
+function_signature: "def median(numbers: List[float]) -> float"
+docstring: |
+    Return median of elements in the list l
+test_cases:
+  - input: [3, 1, 2, 4, 5]
+    output: 3
+  - input: [-10, 4, 6, 1000, 10, 20]
+    output: 15.0
+-/
+-- end_def problem_details
+
+-- start_def problem_spec
+def problem_spec
+-- function signature
+(implementation: List Rat → Rat)
+-- inputs
+(numbers: List Rat) :=
+-- spec
+let spec (result: Rat) :=
+  numbers.length ≥ 1 →
+  (numbers.length % 2 = 1 → result ∈ numbers →
+  let less_count := (numbers.filter (fun x => x < result)).length;
+  let more_count := (numbers.filter (fun x => x > result)).length;
+  let eq_count := (numbers.filter (fun x => x = result)).length;
+  less_count = numbers.length - (eq_count - 1)/2 →
+  more_count = numbers.length - (eq_count - 1)/2) ∧
+  (numbers.length % 2 = 0 →
+  let less_count := (numbers.filter (fun x => x < result)).length;
+  let more_count := (numbers.filter (fun x => x > result)).length;
+  let eq_count := (numbers.filter (fun x => x = result)).length;
+  less_count = numbers.length - eq_count/2 →
+  more_count = numbers.length - eq_count/2);
+-- program termination
+∃ result, implementation numbers = result ∧
+spec result
+-- end_def problem_spec
+
+-- start_def generated_spec
+def generated_spec
+-- function signature
+(impl: List Rat → Rat)
+-- inputs
+(x: List Rat) : Prop :=
+--end_def generated_spec
+--start_def generated_spec_body
+sorry
+--end_def generated_spec_body
+
+
+-- start_def spec_isomorphism
+theorem spec_isomorphism:
+∀ impl,
+(∀ x, problem_spec impl x) ↔
+(∀ x, generated_spec impl x) :=
+-- end_def spec_isomorphism
+-- start_def spec_isomorphism_proof
+sorry
+--end_def spec_isomorphism_proof
+
+-- start_def implementation_signature
+def implementation (numbers: List Rat) : Rat :=
+-- end_def implementation_signature
+-- start_def implementation
+  sorry
+-- end_def implementation
+
+-- Uncomment the following test cases after implementing the function
+-- start_def test_cases
+-- end_def test_cases
+
+-- start_def correctness_definition
+theorem correctness
+(numbers: List Rat)
+: problem_spec implementation numbers
+:=
+-- end_def correctness_definition
+-- start_def correctness_proof
+by
+unfold problem_spec
+let result := implementation numbers
+use result
+simp [result]
+repeat sorry
+-- end_def correctness_proof
