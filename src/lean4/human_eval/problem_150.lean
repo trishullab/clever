9c33966c61a5ab9b0efb69111e0a05cad198c57e
Meghana <sistla.meghana@gmail.com>
2025-04-28 16:54:21 -0700

    Added test for P150

--- src/lean4/human_eval/problem_150.lean
+++ src/lean4/human_eval/problem_150.lean
@@ -61,9 +61,8 @@ sorry
 
 -- Uncomment the following test cases after implementing the function
 -- start_def test_cases
--- #test implementation ([1, 2, 2, -4]: List Int) = (-9: Int)
--- #test implementation ([0, 1]: List Int) = (0: Int)
--- #test implementation ([]: List Int) = none
+-- #test implementation 7 34 12 = 34
+-- #test implementation 15 8 5 = 5
 -- end_def test_cases
 
 
