7ed10ff0b80b239458bfc068b852bf24581300c8
simpissa <matthewzhao420@gmail.com>
2025-05-10 00:44:24 -0500

    fix build error

--- src/lean4/human_eval/problem_132.lean
+++ src/lean4/human_eval/problem_132.lean
@@ -35,7 +35,7 @@ string.toList.all (fun x => x = '(' ∨ x = ')') →
 result = true ↔
   ∃ x : String,
     is_subsequence x.toList string.toList ∧
-    balanced_paren_non_computable x ∧
+    balanced_paren_non_computable x '(' ')' ∧
     2 ≤ count_max_paren_depth x
 -- program termination
 ∃ result, impl string = result ∧
