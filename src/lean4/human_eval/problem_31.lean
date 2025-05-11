8b2ca775625b9506cefa2618aa6b313a0e4500de
Amitayush Thakur <amitayush@utexas.edu>
2025-05-11 21:49:47 +0000

    Fixed prime definition in problem 31

--- src/lean4/human_eval/problem_31.lean
+++ src/lean4/human_eval/problem_31.lean
@@ -31,7 +31,7 @@ def problem_spec
 (n: Nat) :=
 -- spec
 let spec (result: Bool) :=
-  result ↔ (∀ k, 2 ≤ k ∧ k < n ∧ n % k ≠ 0);
+  result ↔ ¬ (∃ k, 2 ≤ k ∧ k < n ∧ n % k = 0);
 -- program termination
 ∃ result,
   implementation n = result ∧
